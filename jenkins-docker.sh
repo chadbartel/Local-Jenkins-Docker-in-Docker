f64579fa28b59e7a0fed386d8aa0aa3e5005faf4
chadbartel <bartel.chad@gmail.com>
2021-07-01 19:25:37 -0700

    add docker sock and chmod, chown

--- jenkins-docker.sh
+++ jenkins-docker.sh
@@ -29,4 +29,8 @@ docker run \
     --publish 50000:50000 \
     --volume jenkins-data:/var/jenkins_home \
     --volume jenkins-docker-certs:/certs/client:ro \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
     myjenkins-blueocean:$IMAGE_TAG
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+sudo chmod 666 /var/run/docker.sock
